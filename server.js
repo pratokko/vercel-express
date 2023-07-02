15cfd34ec52abe2ca6c341971adecb796e423972
pratokko <pratokko3@gmail.com>
2023-07-02 11:37:27 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello man");
+});
+app.get("/blog", (req, res) => {
+  res.send(" this is the blog route");
+});
+
+app.listen(PORT, () => {
+  ` app now running on port ${PORT}`;
+});
